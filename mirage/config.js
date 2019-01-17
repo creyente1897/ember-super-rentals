bb7a6099a292ebe303653861f606e7fc71af95bd
creyente1897 <ayushsrivastava1897@gmail.com>
2019-01-17 23:25:23 +0530

    Added location filter

--- mirage/config.js
+++ mirage/config.js
@@ -1,9 +1,7 @@
 export default function() {
   this.namespace = '/api';
 
-  this.get('/rentals', function() {
-    return {
-      data: [{
+let rentals = [{
         type: 'rentals',
         id: 'grand-old-mansion',
         attributes: {
@@ -39,7 +37,15 @@ export default function() {
           image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
           description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
         }
-      }]
-    };
-  });
+      }];
+      this.get('/rentals', function(db, request) {
+        if(request.queryParams.city !== undefined) {
+          let filteredRentals = rentals.filter(function(i) {
+            return i.attributes.city.toLowerCase().indexOf(request.queryParams.city.toLowerCase()) !== -1;
+          });
+          return { data: filteredRentals };
+        } else {
+          return { data: rentals };
+        }
+      });
 }
--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,7 +4,9 @@ import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import {
   click,
   currentURL,
-  visit
+  visit,
+  fillIn,
+  triggerKeyEvent
 } from '@ember/test-helpers'
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
@@ -36,6 +38,14 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should show details for a selected rental', async function (assert) {
+  });
+
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
 
 });
