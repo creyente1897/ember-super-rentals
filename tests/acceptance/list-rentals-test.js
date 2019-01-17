7e1102f07dd9ef351a7b08f0261a5c20bbdaac3c
creyente1897 <ayushsrivastava1897@gmail.com>
2019-01-17 18:14:58 +0530

    Added contact, about ,rentals/index route to the application

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,37 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import {
+  click,
+  currentURL,
+  visit
+} from '@ember/test-helpers'
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show rentals as the home page', async function (assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redurect automatiallly');
+  });
+
+  test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+
+  test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+
+  test('should list available rentals.', async function (assert) {
+  });
+
+  test('should filter the list of rentals by city.', async function (assert) {
+  });
+
+  test('should show details for a selected rental', async function (assert) {
+  });
+
+});
